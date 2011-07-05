1af1eb173f2e33e893f50df31db7e185d6248a4c
Anton Podviaznikov <podviaznikov@gmail.com>
2011-07-05 22:25:07 +0200

    udpates.

--- server.js
+++ server.js
@@ -19,13 +19,17 @@ app.configure(function(){
 util.log('Server started!');
 app.listen(80);
 
-io.of('/events:last').on('connection',function(socket){
+io.of('/eventsLast').on('connection',function(socket){
     socket.emit('news',{ hello: 'world' });
+    sportsService.getLastFootballEvents(function(er,data){
+        console.log('SPORT EVENTS SHOULD BE SEND');
+        socket.emit('news',data);
+    });
     socket.on('my other event',function (data) {
         console.log(data);
     });
 });
-io.of('/events:now').on('connection',function(socket){
+io.of('/eventsNow').on('connection',function(socket){
     sportsService.getLastFootballEvents(function(er,data){
         socket.emit('news',data);
     });
@@ -34,7 +38,7 @@ io.of('/events:now').on('connection',function(socket){
         console.log(data);
     });
 });
-io.of('/events:next').on('connection',function(socket){
+io.of('/eventsNext').on('connection',function(socket){
     socket.emit('news',{ hello: 'world' });
     socket.on('my other event',function (data) {
         console.log(data);
