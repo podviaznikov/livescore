08fa456276e594a6a7f9160e1aa60826eea199eb
Anton Podviaznikov <podviaznikov@gmail.com>
2011-07-05 21:49:50 +0200

    initial upload.

---
+++ server.js
@@ -0,0 +1,26 @@
+var util=require('util'),
+    express=require('express'),
+    socketIo=require('socket.io'),
+    app=express.createServer(),
+    io=socketIo.listen(app);
+
+app.configure(function(){
+    app.use(express.logger());
+    //component for decoding requests' params
+    app.use(express.bodyParser());
+    //session support
+    app.use(express.cookieParser());
+    app.use(express.session({secret: 'super_hard_session_secret',cookie:{ path: '/', httpOnly: true, maxAge: 14400000000000000 }}));
+    //public folder for static files
+    app.use(express.static(__dirname+'/public'));
+});
+
+util.log('Server started!');
+app.listen(80);
+
+io.sockets.on('connection',function(socket){
+    socket.emit('news',{ hello: 'world' });
+    socket.on('my other event',function (data) {
+        console.log(data);
+    });
+});
