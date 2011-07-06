7bea92c652ec4eb9ecfc3c489fc2fa1166d74cd7
Anton Podviaznikov <podviaznikov@gmail.com>
2011-07-06 21:08:28 +0200

    updates.

--- services/sports.service.js
+++ services/sports.service.js
@@ -8,10 +8,14 @@ var util=require('util'),
 
 fanfeedr.init('9xcjyztzg8d9d24euvfz8fec','basic',true);
 
-scheduler.addAndRunJob('job1','00 */2 * * * *',function(){
+scheduler.addAndRunJob('job1','00 */2 * * * *',getLastSportResults);
+
+function getLastSportResults(){
     fanfeedr.lastSportEvents(fanfeedrData.sports.football,function(err,data){
         if(!err){
-            for(var i=0;i<data.length;i++){
+            var events=[],
+                i=0;
+            for(;i<data.length;i++){
                 var event=data[i],
                     eventId=event.id,
                     eventName=event.name,
@@ -20,18 +24,31 @@ scheduler.addAndRunJob('job1','00 */2 * * * *',function(){
                         id:eventId,
                         date:event.date,
                         home:eventParticipants[0].trim(),
-                        away:eventParticipants[1].trim(),
+                        away:eventParticipants[1].trim()
                     },
+                    events[i]=eventData;
                     eventDataStr=JSON.stringify(eventData);
                store.sadd('lastevents_new',eventDataStr,redis.print);
             }
-            store.sinter('lastevents_new','lastevents_old',function(err,result){
-                util.log('difference between new and old events');
-                util.log(util.inspect(result));
-                store.rename('lastevents_new','lastevents_old',redis.print);
-            });
+            store.sinter('lastevents_new','lastevents_old',handleDifferenceNewLastEvents);
             pub.publish('lastevents','Hi');
         }
     });
-});
+};
+
+function handleDifferenceNewLastEvents(err,results){
+    util.log('difference between new and old events');
+    util.log(util.inspect(results));
+    var i=0;
+    for(;i<2;i++){
+        var event=JSON.parse(results[i]);
+        util.log('Parsed='+util.isnpect(event));
+        fanfeedr.getEventDetails(event.id,function(er,data){
+            event.status=data.status;
+            event.score=data.home_team.score+':'data.away_team.score;
+            store.sadd('lastevents_full',JSON.stringify(event));
+        });
+    }
+    store.rename('lastevents_new','lastevents_old',redis.print);
+};
 util.log('started score results scrapper');
