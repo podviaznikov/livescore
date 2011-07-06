b5ba1db263d6dbacbb49db019512b90a4ce6e8fe
Anton Podviaznikov <podviaznikov@gmail.com>
2011-07-06 17:02:05 +0200

    redis experimenst

---
+++ services/fanfeedr.data.js
@@ -0,0 +1,31 @@
+//sports
+exports.sports={
+    boxing:'1fa67523-cef0-5a64-9e21-9af5631109d5',
+    formula:'6a72f772-9933-5d28-abfb-11fdcfbf7ec7',
+    mlb:'20f0857f-3c43-5f50-acfc-879f838ee853',
+    nascar:'d40e07a0-8ff8-5e23-b7be-2f212e3bd307',
+    nba:'f65226d8-fbf7-5033-a7a0-50de55b57968',
+    nfl:'13962b74-cab5-5d0a-93c8-466b6a3fa342',
+    nhl:'8e5c52c9-b7c0-5343-8141-122e12fdc48f',
+    pga:'92c56b74-7860-58ef-8f3d-92402d0fe0cb',
+    racing:'f6ad29be-2699-5e5e-841c-1c04a2f5dbd4',
+    football:'0a3c27d2-a655-58e4-a49c-9f3c7411c710',
+    tennis:'0f4005bb-b854-5c65-9f28-d564c2e0b7f8'
+};
+exports.footballLeagues=[
+    {"id": "4c941317-f28f-5171-bc19-ce1879939413", "name": "Champions League"},
+    {"id": "7e7974b0-53d3-5432-b3f4-3777c6d9984e", "name": "UEFA"},
+    {"id": "c034e93e-7f70-5ea3-97b5-5f6e09991e45", "name": "Bundesliga"},
+    {"id": "0872ff0e-7708-5b81-bc5e-b7fcc382b3a8", "name": "Eredivisie"},
+    {"id": "4de88ed7-51c3-54c0-80c2-6819e1d13d47", "name": "Ligue 1"},
+    {"id": "b3c0d93e-ce60-5984-ba07-801e4064ed52", "name": "Primera"},
+    {"id": "c9f96582-058d-5bf3-b459-4d98ce3b5aec", "name": "MLS"},
+    {"id": "8e651af0-93cf-5959-8f55-416533f2c411", "name": "Premier League"},
+    {"id": "1b67d32e-ff01-5c36-89dc-67dd64a14bb8", "name": "La Liga"},
+    {"id": "569d24c6-c774-5970-97c7-b3be76091579", "name": "Serie A"},
+    {"id": "6982f90d-2df8-5c0f-8ace-51daa7a50c54", "name": "National Team"}
+];
+//events response sample
+[{"date": "2011-07-06T20:00:00.000000Z", "id": "a1bd0c2a-189b-5a48-8176-f6e4895bdf85", "name": "Toronto FC @ NY Red Bulls"}, {"date": "2011-07-06T20:30:00.000000Z", "id": "20868e77-01ec-55a9-9ddf-7406c0cd553e", "name": "Colorado Rapids @ Sporting Kansas City"}, {"date": "2011-07-06T22:00:00.000000Z", "id": "4277f856-0dac-581d-b34b-9018001a1d11", "name": "Columbus Crew @ Vancouver Whitecaps FC"}, {"date": "2011-07-06T22:30:00.000000Z", "id": "dca14fcc-29e4-5af3-8e1e-76fd70494e65", "name": "SJ Earthquakes @ Chivas USA"}, {"date": "2011-07-09T19:30:00.000000Z", "id": "45982e37-1784-5458-96e7-ded21d6cffad", "name": "Chivas USA @ Sporting Kansas City"}, {"date": "2011-07-09T19:30:00.000000Z", "id": "3b59d1a9-d148-5fa9-9a94-65b200316940", "name": "DC United @ NY Red Bulls"}, {"date": "2011-07-09T20:30:00.000000Z", "id": "42d1e10f-44ac-5fea-9139-ac21ca86019c", "name": "Toronto FC @ Houston Dynamo"}, {"date": "2011-07-09T21:00:00.000000Z", "id": "fec2f17b-a81e-525c-8b00-74d8901c3d27", "name": "Vancouver Whitecaps FC @ Colorado Rapids"}, {"date": "2011-07-09T21:00:00.000000Z", "id": "dc077e1d-27b1-582f-850a-00d24d5e247e", "name": "FC Dallas @ Real Salt Lake"}, {"date": "2011-07-09T22:30:00.000000Z", "id": "b6b19fac-46c1-5e7d-800f-0d2197ed6eca", "name": "Philadelphia Union @ SJ Earthquakes"}, {"date": "2011-07-09T22:30:00.000000Z", "id": "ad5df8aa-dac6-5c2b-996a-4bdd32207b8b", "name": "Chicago Fire @ LA Galaxy"}, {"date": "2011-07-10T16:00:00.000000Z", "id": "637d492c-e970-5335-9187-0f4e5ed847ca", "name": "Seattle Sounders @ Portland Timbers"}, {"date": "2011-07-16T16:00:00.000000Z", "id": "29dbe50f-b523-55b7-ae0b-67c70b09b53c", "name": "Real Salt Lake @ Vancouver Whitecaps FC"}, {"date": "2011-07-16T16:00:00.000000Z", "id": "517762b0-caf2-56a4-8444-dd7a38a73371", "name": "Colorado Rapids @ Seattle Sounders"}, {"date": "2011-07-16T19:30:00.000000Z", "id": "d027c637-74ac-5bcd-9a0e-b041fb7785f0", "name": "SJ Earthquakes @ Columbus Crew"}, {"date": "2011-07-16T20:30:00.000000Z", "id": "926f3f82-9ceb-5618-877c-b76cedf7c5f0", "name": "Portland Timbers @ Chicago Fire"}, {"date": "2011-07-16T20:30:00.000000Z", "id": "a1d1f1ee-bff3-5d7c-bb78-d224a5e0f8ed", "name": "Sporting Kansas City @ Houston Dynamo"}, {"date": "2011-07-16T21:00:00.000000Z", "id": "de128e10-0287-5846-97cd-1ca0a534a415", "name": "DC United @ FC Dallas"}, {"date": "2011-07-16T22:30:00.000000Z", "id": "224de30d-4192-5424-95eb-b901e91bef99", "name": "NY Red Bulls @ Chivas USA"}, {"date": "2011-07-17T19:00:00.000000Z", "id": "45420868-cfc2-5b89-820c-d7e1d625535d", "name": "Philadelphia Union @ NE Revolution"}]
+//event response sample
+{"status": "FINAL", "home_team": {"score": 0, "id": "6bc9afa4-ac53-5d49-b6fd-bde35e627598", "name": "Philadelphia Union"}, "season_year": 2011, "away_team": {"score": 0, "id": "8af7e771-ed64-5c12-a317-54b51087d82d", "name": "Sporting Kansas City"}, "name": "Sporting Kansas City @ Philadelphia Union", "date": "2011-06-22T19:30:00.000000Z", "season_type": "regular", "id": "4c0f9764-d07c-5e21-b6c1-2c01bf5c9e52"}
--- services/sports.service.js
+++ services/sports.service.js
@@ -1,6 +1,27 @@
-var sportsDataRepo=require('./sports.data.repo'),
-    fanfeedr=require('fanfeedr');
+var fanfeedrData=require('./fanfeedr.data'),
+    fanfeedr=require('fanfeedr'),
+    redis=require('redis'),
+    client=redis.createClient();
+
 fanfeedr.init('9xcjyztzg8d9d24euvfz8fec','basic',true);
 exports.getLastFootballEvents=function(callback){
-    fanfeedr.lastSportEvents(sportsDataRepo.sports.football,callback);
+    fanfeedr.lastSportEvents(fanfeedrData.sports.football,callback);
 }
+
+fanfeedr.lastSportEvents(fanfeedrData.sports.football,function(err,data){
+    if(!err){
+        for(var i=0;i<data.length;i++){
+            var event=data[i],
+                eventId=event.id,
+                eventName=event.name,
+                eventParticipants=event.name.split('@'),
+                eventData={
+                    date:event.date,
+                    home:eventParticipants[0].trim(),
+                    away:eventParticipants[1].trim(),
+                };
+
+            client.sadd('lastevents',JSON.stringify(eventData));
+        }
+    }
+});
